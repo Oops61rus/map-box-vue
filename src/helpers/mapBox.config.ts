4a25d7c9b289b1864f1dccfdee27a5d43bb11d99
adenisov <adenisov@phoenixit.ru>
2023-06-21 18:46:02 +0300

    [master] - initial commit: - create mapBox.config.ts (info with coordinates)

---
+++ src/helpers/mapBox.config.ts
@@ -0,0 +1,8 @@
+import type { CoordinatesModelI } from '@/models/coordinates.model';
+
+const coordinates: CoordinatesModelI = {
+  Portland: [-122.676483, 45.523064],
+  Berlin: [13.394, 52.52],
+}
+
+export default coordinates
