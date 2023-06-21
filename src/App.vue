1bec3e78f6aa030327c8576647114bbba556750d
adenisov <adenisov@phoenixit.ru>
2023-06-22 01:34:41 +0300

    [master]: - moved loader

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 <script setup lang="ts">
 import MbHeader from '@/components/layouts/mbHeader.vue';
 import MbMain from '@/components/layouts/mbMain.vue';
-import MbLoader from '@/components/layouts/mbLoader.vue';
+import MbLoader from '@/components/ui/mbLoader.vue';
 </script>
 
 <style scoped>
