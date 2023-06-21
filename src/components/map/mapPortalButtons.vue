cc51b87b30f371c5dfe3e8cee7c1f6fdf78cad34
adenisov <adenisov@phoenixit.ru>
2023-06-21 18:45:19 +0300

    [master] - initial commit: - create ui mapPortalButtons.vue

---
+++ src/components/map/mapPortalButtons.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="portals">
+    <mb-button @click="$emit('onClickButton', 'Portland')">To Portland</mb-button>
+    <mb-button @click="$emit('onClickButton', 'Berlin')">To Berlin</mb-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MbButton from '@/components/ui/mbButton.vue';
+
+export default defineComponent({
+  name: 'map-portal-buttons',
+  components: { MbButton },
+  emits: ['onClickButton'],
+});
+</script>
+
+<style scoped>
+.portals {
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+  display: flex;
+  gap: 10px;
+}
+</style>
