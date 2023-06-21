6565516ba6cf76014963a1abd383309de13dfafe
adenisov <adenisov@phoenixit.ru>
2023-06-22 01:23:37 +0300

    [master]: - create new component mbLoader.vue

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
   <mb-header />
 
   <mb-main />
+
+  <mb-loader />
 </template>
 
 <script setup lang="ts">
 import MbHeader from '@/components/layouts/mbHeader.vue';
 import MbMain from '@/components/layouts/mbMain.vue';
+import MbLoader from '@/components/layouts/mbLoader.vue';
 </script>
 
 <style scoped>
