2a4145696ca0d7052bc81c8430102d8dba0d3819
adenisov <adenisov@phoenixit.ru>
2023-06-21 18:48:35 +0300

    [master] - initial commit: - create component mbMain.vue - add component to App.vue

---
+++ src/components/layouts/mbMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <mb-map-box />
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MbMapBox from '@/components/map/mapBox.vue';
+
+export default defineComponent({
+  name: 'mbMain',
+  components: { MbMapBox },
+});
+</script>
+
+<style scoped>
+</style>
