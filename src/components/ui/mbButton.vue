4199fff66f92cf4e67b7115635ec35980482f353
adenisov <adenisov@phoenixit.ru>
2023-06-21 18:44:14 +0300

    [master] - initial commit: - create ui components mbButton.vue, mbHeaderTitle.vue

---
+++ src/components/ui/mbButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'mb-button',
+  setup() {
+    return {};
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+});
+</script>
+
+<style scoped>
+.button {
+  cursor: pointer;
+  background: white;
+  border: none;
+  border-radius: 6px;
+  font-size: 20px;
+  padding: 4px 8px;
+  transition: all .3s;
+}
+.button:hover {
+  background: #DDD;
+  color: #333;
+}
+</style>
---
+++ src/components/ui/mbHeaderTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="main-title">
+    <slot />
+  </h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'mb-header-title',
+});
+</script>
+
+<style scoped>
+.main-title {
+  font-size: 40px;
+  text-align: center;
+}
+</style>
