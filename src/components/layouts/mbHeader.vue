b6bf86815bfe9a05bbbf7e7f593d2bfd4e37e818
adenisov <adenisov@phoenixit.ru>
2023-06-21 18:44:34 +0300

    [master] - initial commit: - create component mbHeader.vue

---
+++ src/components/layouts/mbHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <header ref="header">
+    <mb-header-title>MapBox</mb-header-title>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MbHeaderTitle from '@/components/ui/mbHeaderTitle.vue';
+
+export default defineComponent({
+  name: 'mbHeader',
+  components: { MbHeaderTitle },
+  data() {
+    return {
+      className: 'welcome-loading'
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.$refs.header.classList.add(this.className)
+    }, 3000)
+  }
+});
+</script>
+
+<style scoped>
+header {
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  background: white;
+  transition: height 1s ease-in-out;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.welcome-loading {
+  height: 50px;
+  background: rgba(255, 255, 255, 0.8);
+}
+</style>
